73c0c847293e5966d56200315c95b172da98729d
internationalwe <tlsghwns1122@gmail.com>
2023-12-13 19:36:47 +0000

    "Del : Augmentation Code"

--- modules/datasets.py
+++ modules/datasets.py
@@ -42,76 +42,6 @@ def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
 
-class BaseAugmentation:
-    """
-    기본적인 Augmentation을 담당하는 클래스
-
-    Attributes:
-        transform (Compose): 이미지를 변환을 위한 torchvision.transforms.Compose 객체
-    """
-
-    def __init__(self, resize, mean, std, **args):
-        """
-        Args:
-            resize (tuple): 이미지의 리사이즈 대상 크지
-            mean (tuple): Normalize 변환을 위한 평균 값
-            std (tuple): Normalize 변환을 위한 표준 값
-        """
-        self.transform = Compose(
-            [
-                Resize(resize, Image.BILINEAR),
-                ToTensor(),
-                Normalize(mean=mean, std=std),
-            ]
-        )
-
-    def __call__(self, image):
-        """
-        이미지에 저장된 transform 적용
-
-        Args:
-            Image (PIL.Image): Augumentation을 적용할 이미지
-
-        Returns:
-            Tensor: Argumentation이 적용된 이미지
-        """
-        return self.transform(image)
-
-
-class AddGaussianNoise(object):
-    """이미지에 Gaussian Noise를 추가하는 클래스"""
-
-    def __init__(self, mean=0.0, std=1.0):
-        self.std = std
-        self.mean = mean
-
-    def __call__(self, tensor):
-        return tensor + torch.randn(tensor.size()) * self.std + self.mean
-
-    def __repr__(self):
-        return self.__class__.__name__ + "(mean={0}, std={1})".format(
-            self.mean, self.std
-        )
-
-
-class CustomAugmentation:
-    """커스텀 Augmentation을 담당하는 클래스"""
-
-    def __init__(self, resize, mean, std, **args):
-        self.transform = Compose(
-            [
-                CenterCrop((320, 256)),
-                Resize(resize, Image.BILINEAR),
-                ColorJitter(0.1, 0.1, 0.1, 0.1),
-                ToTensor(),
-                Normalize(mean=mean, std=std),
-                AddGaussianNoise(),
-            ]
-        )
-
-    def __call__(self, image):
-        return self.transform(image)
-
 
 class MaskLabels(int, Enum):
     """마스크 라벨을 나타내는 Enum 클래스"""
@@ -392,16 +322,10 @@ class TestDataset(Dataset):
     """테스트 데이터셋 클래스"""
 
     def __init__(
-        self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)
+        self, img_paths, transform
     ):
         self.img_paths = img_paths
-        self.transform = Compose(
-            [
-                Resize(resize, Image.BILINEAR),
-                ToTensor(),
-                Normalize(mean=mean, std=std),
-            ]
-        )
+        self.transform = transform
 
     def __getitem__(self, index):
         """인덱스에 해당하는 데이터를 가져오는 메서드"""
